569a0e1ce1d432eed10937f25776dc422c8fb061
Sami Rherbi <sami0771@edu.zealand.dk>
2022-10-09 13:04:15 +0200

    Task 3

---
+++ TicketSystemClassLibrary/MC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystemClassLibrary
+{
+    public class MC
+    {
+        public string Licenseplate { get; set; }
+        public DateTime Date { get; set; }
+
+        public double Price()
+        { return 125; }
+
+        public string VehicleType()
+        { return "MC"; }
+    }
+}
---
+++ TicketSystemClassLibraryTests/MCTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketSystemClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketSystemClassLibrary.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        MC mc = new MC();
+        [TestMethod()]
+        public void PriceTest()
+        {
+            Assert.AreEqual(mc.Price, mc.Price);
+        }
+
+        [TestMethod()]
+        public void VehicleTest()
+        {
+            Assert.AreEqual(mc.VehicleType,mc.VehicleType);
+        }
+    }
+}
